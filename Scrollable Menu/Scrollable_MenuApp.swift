b7876511d4ff8b48fdc61f421f19004dfd1213b5
jacobkosmart <jacobkosmart@gmail.com>
2022-03-05 11:11:17 +0900

    Initial Commit

---
+++ Scrollable Menu/Scrollable_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrollable_MenuApp.swift
+//  Scrollable Menu
+//
+//  Created by Jacob Ko on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct Scrollable_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
